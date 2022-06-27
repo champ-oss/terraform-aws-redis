942d8a0a266cbe0e64409baf1cb23e54a583f268
Phillip Langley <philliplangley@champmbplangley.lan>
2022-06-27 15:45:52 -0500

    initial release of redis

---
+++ redis.tf
@@ -0,0 +1,19 @@
+resource "aws_elasticache_cluster" "this" {
+  cluster_id           = "redis-${var.git}"
+  engine               = "redis"
+  engine_version       = var.engine_version
+  maintenance_window   = var.maintenance_window
+  node_type            = var.node_type
+  num_cache_nodes      = var.num_cache_nodes
+  parameter_group_name = var.parameter_group_name
+  port                 = var.redis_port
+  subnet_group_name    = aws_elasticache_subnet_group.this.name
+  security_group_ids   = ["${aws_security_group.this.id}"]
+  tags                 = merge(local.tags, var.tags)
+}
+
+resource "aws_elasticache_subnet_group" "this" {
+  name        = "${var.git}-sg-${random_string.identifier.result}"
+  description = "private subnets for the elastic cache cluster"
+  subnet_ids  = [var.subnet_ids]
+}
---
+++ variables.tf
@@ -0,0 +1,64 @@
+variable "git" {
+  description = "Name of the Git repo"
+  type        = string
+  default     = "terraform-aws-redis"
+}
+
+variable "tags" {
+  description = "Tags to assign to resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "engine_version" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#engine_version"
+  type        = string
+  default     = "6.2"
+}
+
+variable "parameter_group_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#parameter_group_name"
+  type        = string
+  default     = "default.redis6.2"
+}
+
+variable "node_type" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#node_type"
+  type        = string
+  default     = "cache.t2.micro"
+}
+
+variable "maintenance_window" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#maintenance_window"
+  type        = string
+  default     = "sun:07:00-sun:08:00"
+}
+
+variable "num_cache_nodes" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#num_cache_nodes"
+  type        = number
+  default     = 1
+}
+
+variable "redis_port" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_cluster#port"
+  type        = number
+  default     = 6379
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group#vpc_id"
+}
+
+variable "source_security_group_id" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group_rule#source_security_group_id"
+  default     = ""
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/elasticache_subnet_group#subnet_ids"
+  default     = []
+  type        = list(string)
+}
