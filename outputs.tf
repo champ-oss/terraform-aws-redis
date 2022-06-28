340ec7a8427b91d8274b1c376a82316e0cd61260
Phillip Langley <philliplangley@champmbplangley.lan>
2022-06-28 14:13:43 -0500

    adding endpoint output

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
-output "endpoint" {
-  value = "${aws_elasticache_cluster.this.cache_nodes.0.address}:6379"
+output "reddis_endpoint" {
+  description = "reddis endpoint"
+  value       = "${aws_elasticache_cluster.this.cache_nodes.0.address}:6379"
 }
