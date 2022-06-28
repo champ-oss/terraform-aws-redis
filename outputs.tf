2a9369bb11334e28a9142e8321edc46d1b690015
Phillip Langley <philliplangley@champmbplangley.lan>
2022-06-28 14:23:11 -0500

    resolve linting issue with output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "redis_endpoint" {
-  desciption = "redis endpoint"
-  value      = aws_elasticache_cluster.this.cache_nodes[0].address
+  description = "redis endpoint"
+  value       = aws_elasticache_cluster.this.cache_nodes[0].address
 }
 
 output "redis_port" {
-  desciption = "redis port"
-  value      = aws_elasticache_cluster.this.cache_nodes[0].port
+  description = "redis port"
+  value       = aws_elasticache_cluster.this.cache_nodes[0].port
 }
