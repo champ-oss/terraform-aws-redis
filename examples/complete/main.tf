940341a393ad7746ae457f0b70cba90d897611f2
Justin Reeves <reeves122@gmail.com>
2023-03-07 12:49:43 -0600

    use name instead of git var

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -8,7 +8,7 @@ locals {
 
 module "vpc" {
   source                   = "github.com/champ-oss/terraform-aws-vpc?ref=v1.0.45-92e9a14"
-  git                      = local.git
+  name                     = local.git
   availability_zones_count = 2
   retention_in_days        = 1
 }
