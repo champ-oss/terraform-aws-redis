bb7f2841fe4c7c107b8101bf53ba2d83bd00022e
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-04-21 11:06:56 -0500

    adding password ssm name output var

--- outputs.tf
+++ outputs.tf
@@ -18,3 +18,9 @@ output "redis_port" {
   value       = aws_elasticache_replication_group.this.port
   description = "redis port"
 }
+
+output "password_ssm_name" {
+  description = "https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ssm_parameter#name"
+  value       = aws_ssm_parameter.this.name
+}
+
