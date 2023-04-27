d2f37bcb5b44b3c7ec4eba981688155fd998e743
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-04-27 12:10:32 -0500

    testing redis client in lambda

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -69,6 +69,6 @@ resource "random_string" "identifier" {
 
 module "hash" {
   source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.12-fc3bb87"
-  path     = "${path.module}/.."
+  path     = "${path.module}/../.."
   fallback = ""
 }
