c1ce36e13efbe5990ea5fc7739b2a318bae070a0
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-04-27 15:54:54 -0500

    testing client connect

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -52,4 +52,35 @@ resource "random_string" "this" {
   number  = true
 }
 
+module "lambda" {
+  source              = "github.com/champ-oss/terraform-aws-lambda.git?ref=v1.0.115-77403a9"
+  git                 = "terraform-aws-redis-client"
+  name                = "redis-client-${random_string.this.result}"
+  description         = "redis client lambda function"
+  enable_cw_event     = true
+  schedule_expression = "rate(1 minute)"
+  enable_vpc          = true
+  private_subnet_ids  = data.aws_subnets.private.ids
+  sync_image          = true
+  sync_source_repo    = "champtitles/redis-client"
+  ecr_name            = "redis-client-${random_string.this.result}"
+  ecr_tag             = module.hash.hash
+  timeout             = 60
+  vpc_id              = data.aws_vpcs.this.ids[0]
+  environment = {
+    "REDIS_HOST" = module.this.redis_cluster_endpoint,
+    "REDIS_PORT" = module.this.redis_port,
+    "REDIS_AUTH" = module.this.redis_default_password
+  }
+}
 
+resource "time_sleep" "this" {
+  depends_on      = [module.lambda]
+  create_duration = "300s"
+}
+
+module "hash" {
+  source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.12-fc3bb87"
+  path     = "${path.module}/../.."
+  fallback = ""
+}
