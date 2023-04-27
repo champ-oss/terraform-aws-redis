b93eca3dd12b104d6ef19f10df1d22182815ad59
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-04-27 11:33:15 -0500

    testing redis client in lambda

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -31,3 +31,44 @@ module "this" {
   source_security_group_id = aws_security_group.test.id
   subnet_ids               = data.aws_subnets.private.ids
 }
+
+module "lambda" {
+  source                         = "github.com/champ-oss/terraform-aws-lambda.git?ref=v1.0.115-77403a9"
+  git                            = "terraform-aws-redis-client"
+  name                           = "redis-client-${random_string.identifier.result}"
+  description                    = "redis client lambda function"
+  enable_cw_event     = true
+  schedule_expression = "rate(1 minutes)"
+  enable_vpc          = true
+  private_subnet_ids  = data.aws_subnets.private.ids
+  sync_image          = true
+  sync_source_repo    = "champtitles/redis-client"
+  ecr_name            = "redis-client-${random_string.identifier.result}"
+  ecr_tag             = module.hash.hash
+  timeout             = 60
+  vpc_id              = data.aws_vpcs.this.ids[0]
+  environment = {
+    "REDIS_HOST" = module.this.redis_cluster_endpoint,
+    "REDIS_PORT" = module.this.redis_port,
+    "REDIS_AUTH" = module.this.redis_default_password
+  }
+}
+
+resource "time_sleep" "this" {
+  depends_on      = [module.lambda]
+  create_duration = "300s"
+}
+
+resource "random_string" "identifier" {
+  length  = 5
+  special = false
+  upper   = false
+  lower   = true
+  numeric = false
+}
+
+module "hash" {
+source   = "github.com/champ-oss/terraform-git-hash.git?ref=v1.0.12-fc3bb87"
+path     = "${path.module}/.."
+fallback = ""
+}
---
+++ examples/python/redis_client.py
@@ -0,0 +1,45 @@
+import json
+import redis
+import sys
+import os
+import logging
+
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
+def lambda_handler(event, context):
+    redis_conn = None
+    redis_endpoint = None
+    redis_port = None
+    redis_auth = None
+    
+    if "REDIS_HOST" in os.environ and "REDIS_AUTH" in os.environ and "REDIS_PORT" in os.environ:
+        redis_endpoint = os.environ["REDIS_HOST"]
+        redis_port = os.environ["REDIS_PORT"]
+        redis_auth = os.environ["REDIS_AUTH"]
+        log.debug("redis: " + redis_endpoint)
+    else:
+        log.debug(" REDIS_HOST REDIS_PORT REDIS_AUTH configuration not set !")
+        return {
+            'statusCode': 500
+        }
+        
+    try:
+        log.debug('#Starting redis connection')
+        redis_conn = redis.StrictRedis(host=redis_endpoint, port=redis_port, db=0, ssl=True, password=redis_auth)
+        log.debug('connected')
+        redis_conn.set("foo", "bar")
+        log.debug(redis_conn.get("foo"))
+    except Exception as ex:
+        log.debug("failed to connect to redis:", ex.__class__, " occurred")
+        return {
+            'statusCode': 500
+        }
+    finally:
+        del redis_conn
+        
+    return {
+        'statusCode': 200
+    }
